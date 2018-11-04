ca10ebe7e6b7c015eb0b0c048433c37618d15eab
Chris Glover <c.d.glover@gmail.com>
2018-11-04 16:44:55 -0500

    Use latest trace.

--- src/boost_assert.cpp
+++ src/boost_assert.cpp
@@ -1,5 +1,5 @@
 // ****************************************************************************
-// boost_assert.h
+// trace/src/boost_assert.cpp
 //
 // Adapters for using boost assert within daily
 //
@@ -11,28 +11,28 @@
 
 #include "daily/trace/debugbreak.hpp"
 #include "daily/trace/log.hpp"
+#include <boost/stacktrace/stacktrace.hpp>
+#include <stdexcept>
 
 // ----------------------------------------------------------------------------
 // Required when linking against a boost with boost assert enabled.
 namespace boost {
-void assertion_failed(
-    char const* expr, char const* function, char const* file, long line) {
-  LOG() << "***** Internal Program Error - assertion (" << expr
-        << ") failed in " << function << ":\n"
-        << file << '(' << line << ")" << std::endl;
-  DEBUG_BREAK();
-}
-
 void assertion_failed_msg(
     char const* expr,
     char const* msg,
     char const* function,
-    char const* file,
-    long line) {
-  LOG() << "***** Internal Program Error - assertion (" << expr
-        << ") failed in " << function << ":\n"
-        << file << '(' << line << "): " << msg << std::endl;
+    char const* /*file*/,
+    long /*line*/) {
+  daily::trace::LOG() << "Expression '" << expr << "' is false in function '"
+                      << function << "': " << (msg ? msg : "<...>") << ".\n"
+                      << "Backtrace:\n"
+                      << ::boost::stacktrace::stacktrace() << '\n';
   DEBUG_BREAK();
+}
+
+void assertion_failed(
+    char const* expr, char const* function, char const* file, long line) {
+  ::boost::assertion_failed_msg(expr, 0 /*nullptr*/, function, file, line);
 }
 
 } // namespace boost
